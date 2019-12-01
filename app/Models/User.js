bd26b10d7e083f8c588513754587041fa0bace1a
Charles Loureiro de Deus <charliedeus@gmail.com>
2019-12-01 19:04:18 -0300

    Saving relationships

--- app/Models/User.js
+++ app/Models/User.js
@@ -17,6 +17,10 @@ class User extends Model {
     })
   }
 
+  addresses () {
+    return this.hasMany('App/Models/UserAdress')
+  }
+
   /**
    * A relationship on tokens is required for auth to
    * work. Since features like `refreshTokens` or
